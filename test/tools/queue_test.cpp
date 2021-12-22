d0d2f1f47c88cee780ab02c660cdbe197edbc5d7
yankf <981115098@qq.com>
2021-12-22 10:15:25 +0800

    feat : add g_test code

---
+++ test/tools/queue_test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "queue.h"
+#include "ring_queue.h"
+
+#include "gtest/gtest.h"
+
+using namespace tools;
+
+// todo
+// https://www.jianshu.com/p/c7c702c0abb9
+
+TEST(test, test)
+{
+    EXPECT_EQ(1+1, 2);
+}
+
